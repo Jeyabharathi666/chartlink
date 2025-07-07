76a8950671f50b1f5df970317a2696f1fd81baa5
Jeyabharathi666 <saravananjeyabharathi@gmail.com>
2025-07-07 10:44:49 +0530

    Update new.py

--- new.py
+++ new.py
@@ -13,7 +13,7 @@ from concurrent.futures import ThreadPoolExecutor, wait
 Charting_Link = "https://chartink.com/screener/"
 Charting_url = 'https://chartink.com/screener/1-longtrend-ve'
 
-spreadsheet_name = "pappu"
+spreadsheet_name = "SUN1"
 worksheet_letter = "p"
 total_no_of_worksheet = 29
 
